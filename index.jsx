fae2b546e6f7581a501300e8496755b0b7ee9295
Chris Pilcher <christopher.a.pilcher@gmail.com>
2017-03-20 22:03:08 -0400

    Started building out the React components that make up the site

---
+++ index.jsx
@@ -0,0 +1,13 @@
+// Page represents the page as a whole, much like App usually represents an application
+class Page extends React.Component{
+    constructor(props){
+        super(props);
+    }
+}
+
+// Section will represent the individual sections of the site (About me, portfolio, etc)
+class Section extends React.Component{
+    constructor(props){
+        super(props);
+    }
+}
